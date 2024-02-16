5a2792f4b95aef1b4289f6907e38c9530ce21737
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-02-16 08:04:48 -0300

    Update leitor.cpp

--- leitorcsv_cpp/leitor.cpp
+++ leitorcsv_cpp/leitor.cpp
@@ -55,10 +55,11 @@ vector<Dados> ler_csv(string nome_arquivo) {
             dados.push_back(d);                      // Adiciona o objeto d ao vetor de dados
         }
         arquivo.close();                             // Fecha o arquivo
-    } else {                                                // Se o arquivo não foi aberto com sucesso
-cout << "Erro ao abrir o arquivo " << nome_arquivo << endl; // Imprime uma mensagem de erro
-}
-return dados;// Retorna o vetor de dados
+    }
+    else {                                                // Se o arquivo não foi aberto com sucesso
+        cout << "Erro ao abrir o arquivo " << nome_arquivo << endl; // Imprime uma mensagem de erro
+    }
+    return dados;// Retorna o vetor de dados
 }
 
 // Define uma função para mostrar os dados em formato de tabela
@@ -66,13 +67,13 @@ void mostrar_tabela(vector<Dados> dados) {
     cout << "Nome\tCpfCnpj\tTitulo\tDoc\tParc\tVlr\tVenc\tcdTipoCondicao\n"; // Imprime o cabeçalho da tabela
     for (auto d : dados) {                                                   // Percorre o vetor de dados
         cout << d.Cliente << "\t"                                            // Imprime os dados
-        << d.CPF_CNPJ << "\t" 
-        << d.Titulo << "\t" 
-        << d.Doc << "\t" 
-        << d.Parc << "\t" 
-        << d.Vlr << "\t" 
-        << d.Venc << "\t" 
-        << d.cdTipoCondicao << "\t";
+            << d.CPF_CNPJ << "\t"
+            << d.Titulo << "\t"
+            << d.Doc << "\t"
+            << d.Parc << "\t"
+            << d.Vlr << "\t"
+            << d.Venc << "\t"
+            << d.cdTipoCondicao << "\t";
 
         cout << "\n";                                                         // Quebra a linha
     }
@@ -82,7 +83,7 @@ void mostrar_tabela(vector<Dados> dados) {
 void remover_AT(vector<Dados>& dados) {
     dados.erase(remove_if(dados.begin(), dados.end(), [](Dados d) {
         return d.cdTipoCondicao == "AT"; // Retorna verdadeiro se o código do tipo de condição for 'AT'
-    }), dados.end());                    // Remove os elementos que satisfazem a condição
+        }), dados.end());                    // Remove os elementos que satisfazem a condição
 }
 
 // Define uma função para remover os clientes que estão no segundo arquivo do primeiro arquivo
@@ -90,8 +91,8 @@ void remover_clientes(vector<Dados>& dados1, vector<Dados> dados2) {
     dados1.erase(remove_if(dados1.begin(), dados1.end(), [&dados2](Dados d1) {
         return any_of(dados2.begin(), dados2.end(), [d1](Dados d2) {
             return d1.Cliente == d2.Cliente; // Retorna verdadeiro se o nome do cliente for igual
-        });                                  // Retorna verdadeiro se algum elemento do segundo vetor satisfazer a condição
-    }), dados1.end());                       // Remove os elementos que satisfazem a condição
+            });                                  // Retorna verdadeiro se algum elemento do segundo vetor satisfazer a condição
+        }), dados1.end());                       // Remove os elementos que satisfazem a condição
 }
 
 // Define uma função para criar um novo arquivo csv com a solução da operação
@@ -100,18 +101,19 @@ void criar_csv(vector<Dados> dados, string nome_arquivo) {
     if (arquivo.is_open()) {                                                                         // Verifica se o arquivo foi aberto com sucesso
         arquivo << "Nome,CPF_CNPJ\tTitulo\tDocumento\tParcela\tValor\tVencimento\tcdTipoCondicao\n"; // Escreve o cabeçalho do arquivo
         for (auto d : dados) {                                                                       // Percorre o vetor de dados
-            arquivo << d.Cliente << "|"  
-            << d.CPF_CNPJ << "|" 
-            << d.Titulo << "|" 
-            << d.Doc << "|"
-            << d.Parc << "|" 
-            << d.Vlr << "|" 
-            << d.Venc << "|"
-            << d.cdTipoCondicao << ",";
+            arquivo << d.Cliente << "|"
+                << d.CPF_CNPJ << "|"
+                << d.Titulo << "|"
+                << d.Doc << "|"
+                << d.Parc << "|"
+                << d.Vlr << "|"
+                << d.Venc << "|"
+                << d.cdTipoCondicao << ",";
             arquivo << "\n"; // Quebra a linha
         }
         arquivo.close(); // Fecha o arquivo
-    } else {                                                        // Se o arquivo não foi aberto com sucesso
+    }
+    else {                                                        // Se o arquivo não foi aberto com sucesso
         cout << "Erro ao criar o arquivo " << nome_arquivo << endl; // Imprime uma mensagem de erro
     }
 }
@@ -127,16 +129,16 @@ int main() {
 
     vector<Dados> dados1 = ler_csv(nome_arquivo1); // Lê o primeiro arquivo csv e armazena os dados em um vetor
     vector<Dados> dados2 = ler_csv(nome_arquivo2); // Lê o segundo arquivo csv e armazena os dados em outro vetor
-    
+
     cout << "Dados do primeiro arquivo antes da operação:\n"; // Imprime uma mensagem
     mostrar_tabela(dados1);           // Mostra os dados do primeiro arquivo em formato de tabela
     remover_AT(dados1);               // Remove as linhas onde a coluna cdTipoCondicao é 'AT'
     remover_clientes(dados1, dados2); // Remove os clientes que estão no segundo arquivo do primeiro arquivo
-    
+        
     cout << "Dados do primeiro arquivo depois da operação:\n"; // Imprime outra mensagem
     mostrar_tabela(dados1);           // Mostra os dados do primeiro arquivo em formato de tabela
     criar_csv(dados1, nome_arquivo3); // Cria um novo arquivo csv com a solução da operação
-    
+
     cout << "Novo arquivo csv criado com sucesso: " << nome_arquivo3 << endl; // Imprime uma mensagem de confirmação
     system("pause");
     return 0; // Encerra o programa
